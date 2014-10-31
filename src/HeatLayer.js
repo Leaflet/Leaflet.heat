948d40e4b58c099abbfb0f9d463dab743a4d136a
Mateusz Juraszek <mateusz@webtigers.eu>
2014-10-31 14:21:05 +0100

    fix point intensity

--- src/HeatLayer.js
+++ src/HeatLayer.js
@@ -141,7 +141,7 @@ L.HeatLayer = L.Class.extend({
                 x = Math.floor((p.x - offsetX) / cellSize) + 2;
                 y = Math.floor((p.y - offsetY) / cellSize) + 2;
 
-                k = (this._latlngs[i].alt || 1) * v;
+                k = (this._latlngs[i][2] || 1) * v;
 
                 grid[y] = grid[y] || [];
                 cell = grid[y][x];
