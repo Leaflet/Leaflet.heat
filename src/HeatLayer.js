997ee85a3e636addeef6c9a8450780000c96fd47
Mateusz Juraszek <mateusz@webtigers.eu>
2014-11-03 10:35:27 +0100

    fix alt for Array of Arrays and Array of LatLngs usage

--- src/HeatLayer.js
+++ src/HeatLayer.js
@@ -141,7 +141,8 @@ L.HeatLayer = L.Class.extend({
                 x = Math.floor((p.x - offsetX) / cellSize) + 2;
                 y = Math.floor((p.y - offsetY) / cellSize) + 2;
 
-                k = (this._latlngs[i][2] || 1) * v;
+                var alt = isNaN(this._latlngs[i].alt) ? (isNaN(this._latlngs[i][2]) ? 1 : this._latlngs[i][2]) : this._latlngs[i].alt
+                k = alt * v;
 
                 grid[y] = grid[y] || [];
                 cell = grid[y][x];
